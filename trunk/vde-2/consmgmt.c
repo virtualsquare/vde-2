53aa6912c19d43d1aa686e452c8498f1cbbfba30
rd235 <rd235@d37a7db1-d92d-0410-89df-f68f52f87b57>
2007-05-19 13:17:45 +0000

    Connection banner changed (to avoid a problem with automatic mgmt)

--- trunk/vde-2/consmgmt.c
+++ trunk/vde-2/consmgmt.c
@@ -52,7 +52,7 @@ static unsigned int mgmt_ctl=-1;
 static unsigned int mgmt_data=-1;
 static int mgmt_mode = 0600;
 static char *mgmt_socket = NULL;
-static char header[]="VDE switch V.%s\n(C) Virtual Square Team (leader: R. Davoli) 2005,2006,2007 - GPLv2\n";
+static char header[]="VDE switch V.%s\n(C) Virtual Square Team (coord. R. Davoli) 2005,2006,2007 - GPLv2\n";
 static char prompt[]="\nvde: ";
 
 static struct comlist *clh=NULL;
