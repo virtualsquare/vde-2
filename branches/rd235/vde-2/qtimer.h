f2e78a22280ca541899f873bcd01367fd26c0cc7
rd235 <rd235@d37a7db1-d92d-0410-89df-f68f52f87b57>
2011-03-14 14:06:04 +0000

    old experimental branch deleted

--- branches/rd235/vde-2/qtimer.h
+++
@@ -1,9 +0,0 @@
-#ifndef _QTIMER_H
-#define _QTIMER_H
-time_t qtime(); // returns global time (faster than time())
-void qtime_csenter();
-void qtime_csexit();
-unsigned int qtimer_add(time_t period,int times,void (*call)(),void *arg);
-void qtimer_del(unsigned int n);
-void qtimer_init();
-#endif
