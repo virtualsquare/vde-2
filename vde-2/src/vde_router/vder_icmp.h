f1880e94e917f6d1165da4cd3cabbd1d34657bb2
Daniele Lacamera <danielinux@users.noreply.github.com>
2011-12-15 17:39:58 +0000

    Removed erroneously added vde_router (should have been a commit to its branch)

--- vde-2/src/vde_router/vder_icmp.h
+++
@@ -1,8 +0,0 @@
-#ifndef __VDER_ICMP
-#define __VDER_ICMP
-int vder_icmp_service_unreachable(uint32_t dst, uint8_t *foot);
-int vder_icmp_host_unreachable(uint32_t dst, uint8_t *foot);
-int vder_icmp_recv(struct vde_buff *vdb);
-int vder_icmp_filter(uint32_t dst, uint8_t *foot);
-int vder_icmp_ttl_expired(uint32_t dst, uint8_t *foot);
-#endif
