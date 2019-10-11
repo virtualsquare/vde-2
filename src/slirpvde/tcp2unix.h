afa0ae755bd6f40302c449a370844854095d8d1d
Daniele Lacamera <dan@danielinux.net>
2019-10-11 10:58:17 +0200

    Attempt to fix UNIX_PATH_MAX on non-linux

--- src/slirpvde/tcp2unix.h
+++ src/slirpvde/tcp2unix.h
@@ -5,8 +5,11 @@
 #ifndef _TCP2UNIX_H
 #define _TCP2UNIX_H
 
+#include <sys/socket.h>
+#include <sys/un.h>
+
 #ifndef UNIX_PATH_MAX
-#define UNIX_PATH_MAX 108
+#define UNIX_PATH_MAX (sizeof(((struct sockaddr_un*)0)->sun_path))
 #endif
 
 extern int tcp2unix_check;
